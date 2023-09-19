ef3f6de6dfdf994205491388ae1f47a29d46f7bc
Pieter Svenson <whimxiqal@gmail.com>
2023-09-18 23:13:00 -0700

    cleanup

--- paper/src/main/java/net/whimxiqal/mantle/paper/PaperMantleCommand.java
+++ paper/src/main/java/net/whimxiqal/mantle/paper/PaperMantleCommand.java
@@ -22,30 +22,6 @@
  * SOFTWARE.
  */
 
-/*
- * MIT License
- *
- * Copyright (c) Pieter Svenson
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 package net.whimxiqal.mantle.paper;
 
 import java.util.Arrays;
@@ -96,13 +72,13 @@ final class PaperMantleCommand extends Command {
   }
 
   @Override
-  public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+  public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, String[] args) {
     return mantleCommand.process(convertSender(sender), String.join(" ", Arrays.asList(args))).type()
         == CommandResult.Type.SUCCESS;
   }
 
   @Override
-  public @NotNull List<String> tabComplete(CommandSender sender, String alias, String[] args) throws IllegalArgumentException {
+  public @NotNull List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, String[] args) throws IllegalArgumentException {
     return mantleCommand.complete(convertSender(sender), String.join(" ", Arrays.asList(args)));
   }
 
